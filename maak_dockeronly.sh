682947792be21b95d8f25b97a942442aa10fa476
Nikolas Garofil <nikolas@garofil.be>
2018-12-21 02:16:41 +0100

    dockeronlylogin fix

--- maak_dockeronly.sh
+++ maak_dockeronly.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 docker run -v /root:/root --rm compileerimg sh -c 'cd /root && gcc -Wall -Werror -Os -o dockeronlylogin dockeronlylogin.c && rm dockeronlylogin.c'
 echo "dockeronly:x:65533:65533:dockeronly:/:/bin/dockeronlylogin" >> /etc/passwd
-mv /root/dockeronly /bin/
+mv /root/dockeronlylogin /bin/
 rm /root/maak_dockeronly.sh
